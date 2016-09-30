8611f0d7230bbd87c16160261b736a4821e88ec3
Grant Brady <bragr@bradphi.com>
2016-09-29 23:35:48 -0700

    Reorder, fixn names for scoping, and start documenting

--- gn2/net.go
+++ gn2/net.go
@@ -8,43 +8,87 @@ import (
 	"sort"
 )
 
-type (
-	neuron    []float64
-	nLayer    []neuron
-	NeuralNet []nLayer
-)
+//
+// Base neuron type
+// The slice consists of n weights. The n+1 element is the neuron bias
+type neuron []float64
 
-func NewNeuron(numInputs int64) neuron {
+// Basic neuron factory. Creates a neuron with numInputs input with randomized
+// input weights and neuron bias.
+func newNeuron(numInputs int64) neuron {
 	var n neuron
+	// One extra for the bias
 	for i := int64(0); i < numInputs+1; i++ {
 		n = append(n, randWeight())
 	}
 	return n
 }
 
-func NewNeuronLayer(numNeurons int64, numInputs int64) nLayer {
+// Get the bias of the neuron
+func (n neuron) bias() float64 {
+	return n[len(n)-1]
+}
+
+// Calculate the output of the neuron for a given set of inputs
+func (n neuron) updateNeuron(inputs []float64) float64 {
+	var output float64 = 0.0
+	for i := 0; i < len(n)-1; i++ {
+		output += n[i] * inputs[i]
+	}
+	output -= n.bias()
+	return sigmoid(output)
+}
+
+//
+// layer of neurons
+// A simple list of neurons
+type nLayer []neuron
+
+// Basic neuron layer factory. Create numNeurons neurons each taking numInputs
+// inputs
+func newNeuronLayer(numNeurons, numInputs int64) nLayer {
 	var layer nLayer
 	for i := int64(0); i < numNeurons; i++ {
-		layer = append(layer, NewNeuron(numInputs))
+		layer = append(layer, newNeuron(numInputs))
 	}
 	return layer
 }
 
-func NewNeuralNet(numInputs int64, numOutputs int64, numHiddenLayers int64, numNeuronsPerLayer int64) neuralNet {
-	var net neuralNet
+// Calculate the output of each neuron in the layer for the given set of inputs
+func (layer nLayer) updateLayer(inputs []float64) []float64 {
+	var output []float64
+	for _, n := range layer {
+		output = append(output, n.updateNeuron(inputs))
+	}
+	return output
+}
+
+//
+// a full neural net
+// A simple list of neuron layers
+type NeuralNet []nLayer
+
+// Neural net factory. Creates a net that takes numInputs inputs, numOutputs
+// outputs, with numHiddenLayers hidden layers, and numNeuronPerLayer neurons
+// in the hidden layers
+func NewNeuralNet(numInputs, numOutputs, numHiddenLayers, numNeuronsPerLayer) NeuralNet {
+	var net NeuralNet
 	if numHiddenLayers > 0 {
-		net = append(net, NewNeuronLayer(numNeuronsPerLayer, numInputs))
+		net = append(net, newNeuronLayer(numNeuronsPerLayer, numInputs))
 		for i := int64(0); i < numHiddenLayers-1; i++ {
-			net = append(net, NewNeuronLayer(numNeuronsPerLayer, numNeuronsPerLayer))
+			net = append(net, newNeuronLayer(numNeuronsPerLayer, numNeuronsPerLayer))
 		}
-		net = append(net, NewNeuronLayer(numOutputs, numNeuronsPerLayer))
+		net = append(net, newNeuronLayer(numOutputs, numNeuronsPerLayer))
 	} else {
-		net = append(net, NewNeuronLayer(numOutputs, numInputs))
+		net = append(net, newNeuronLayer(numOutputs, numInputs))
 	}
 	return net
 }
 
-func (net neuralNet) getWeights() []float64 {
+// Return a list containing all the edge weights and neuron biases based on a
+// simple serializations of the nested lists. This does the opposite of
+// SetWeights()
+func (net NeuralNet) GetWeights() []float64 {
 	weights := make([]float64, 0)
 
 	for _, layer := range net {
@@ -57,7 +101,9 @@ func (net neuralNet) getWeights() []float64 {
 	return weights
 }
 
-func (net neuralNet) setWeights(newWeights []float64) {
+// Set the edge weights and neuron biases by a simple deserialization of a list
+// weights to the nested list. This does the opposite of GetWeights()
+func (net NeuralNet) SetWeights(newWeights []float64) {
 	index := 0
 	for l, _ := range net {
 		for n, _ := range net[l] {
@@ -69,7 +115,8 @@ func (net neuralNet) setWeights(newWeights []float64) {
 	}
 }
 
-func (net neuralNet) printNet() {
+// Print all the neuron layers, nerons, edge weights, and neuron biases
+func (net NeuralNet) PrintNet() {
 	for c, layer := range net {
 		fmt.Printf("Layer: %d (%d neurons)\n", c, len(layer))
 		for i, neuron := range layer {
@@ -85,28 +132,8 @@ func (net neuralNet) printNet() {
 	}
 }
 
-func (n neuron) bias() float64 {
-	return n[len(n)-1]
-}
-
-func (n neuron) updateNeuron(inputs []float64) float64 {
-	var output float64 = 0.0
-	for i := 0; i < len(n)-1; i++ {
-		output += n[i] * inputs[i]
-	}
-	output -= n.bias()
-	return sigmoid(output)
-}
-
-func (layer nLayer) updateLayer(inputs []float64) []float64 {
-	var output []float64
-	for _, n := range layer {
-		output = append(output, n.updateNeuron(inputs))
-	}
-	return output
-}
-
-func (net neuralNet) update(inputs []float64) []float64 {
+// Return the output of a network for a given input
+func (net NeuralNet) Update(inputs []float64) []float64 {
 	var outputs []float64
 
 	for _, layer := range net {
@@ -117,17 +144,17 @@ func (net neuralNet) update(inputs []float64) []float64 {
 	return outputs
 }
 
-func (net neuralNet) mutate(mutationRate, maxPerturbation float64, numInputs, numOutputs, numHiddenLayers, numNeuronsPerLayer int64) neuralNet {
+func (net NeuralNet) Mutate(mutationRate, maxPerturbation float64, numInputs, numOutputs, numHiddenLayers, numNeuronsPerLayer int64) neuralNet {
 	mutatedNet := NewNeuralNet(numInputs, numOutputs, numHiddenLayers, numNeuronsPerLayer)
 
-	genome := net.getWeights()
+	genome := net.GetWeights()
 
 	for i, _ := range genome {
 		if rand.Float64() < mutationRate {
 			genome[i] += randWeight() * maxPerturbation
 		}
 	}
-	mutatedNet.setWeights(genome)
+	mutatedNet.SetWeights(genome)
 
 	return mutatedNet
 }

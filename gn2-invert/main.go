bcfdb3dc740e8b4ca08426c5d6f1b40b9dd2a189
Grant Brady <bragr@bradphi.com>
2018-08-12 21:46:46 -0700

    impove test

--- gn2-invert/main.go
+++ gn2-invert/main.go
@@ -16,6 +16,10 @@ func genTraingingData(dataSize int64) (inputs, outputs [][]float64) {
 	for i := int64(0); i < dataSize; i++ {
 		outputs = append(outputs, []float64{1.0 - inputs[i][0]})
 	}
+	inputs = append(inputs, []float64{1.0})
+	outputs = append(outputs, []float64{0.0})
+	inputs = append(inputs, []float64{0.0})
+	outputs = append(outputs, []float64{1.0})
 	return inputs, outputs
 }
 
@@ -24,21 +28,22 @@ func main() {
 	gn2.SeedRand()
 
 	// Make a new "species" to train on
-	species := gn2.NewSpecies(20, 1, 1, 4, 15)
+	species := gn2.NewSpecies(20, 1, 1, 6, 20)
+	inputs, outputs := genTraingingData(2000)
 
 	// Train the species
 	for i := 0; i < 1000; i++ {
 		if i%10 == 0 {
 			fmt.Printf(".")
 		}
-		species.Compete(genTraingingData(1000))
-		species.Breed(5, 3, 0)
+		species.Compete(inputs, outputs)
+		species.Breed(3, 3, 1)
 	}
 	fmt.Println()
 
 	// Print the results for the winner
 	species[0].Net.PrintNet()
-	inputs, outputs := genTraingingData(1000)
+	inputs, outputs = genTraingingData(1000)
 	for i, input := range inputs {
 		output := species[0].Net.Update(input)[0]
 		fmt.Printf("Input: %f, Output: %f, Answer: %f Difference: %f%%\n", input[0], output, outputs[i][0], output/outputs[i][0])
